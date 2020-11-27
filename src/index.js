c8eadc074cf251a43b96bb0d0196a3660b0259af
david <puho999@hotmail.com.tw>
2020-11-27 20:25:56 +0800

    blog fix

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 // 引入主題(可設定類似 mixin 的全域變數供使用
 //  import { ThemeProvider } from "styled-components";
-//  import BoardApp from './components/Board/App'
+import BoardApp from './components/Board/App'
 // week21
-//  import TodosApp from "./components/Todos/App";
+import TodosApp from "./components/Todos/App";
 import GobangApp from "./components/Gobang/App";
 //  import FormApp from "./components/Form/App";
 // week22
@@ -19,7 +19,13 @@ ReactDOM.render(
   <Router>
     <Switch>
       {/* blog 下層會再使用 router ，此層不可設為 exact，因會由上往下逐層篩選 */}
-      <Route path="/GobangApp">
+      <Route exact path="/BoardApp">
+        <BlogApp />
+      </Route>
+      <Route exact path="/TodosApp">
+        <GobangApp />
+      </Route>
+      <Route exact path="/GobangApp">
         <GobangApp />
       </Route>
       <Route path="/BlogApp">
